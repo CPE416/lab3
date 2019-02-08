98004ea5e3c05a28b1d115c380d095f446c55dc8
Spencer Shaw <sfshaw@calpoly.edu>
2019-02-08 15:38:11 -0800

    refine tests

--- test_net.c
+++ test_net.c
@@ -8,23 +8,27 @@
 #include "line_follow_pid.h"
 
 
-#define LEARNING_RATE (0.002)
+#define LEARNING_RATE (0.0002)
 
-#define MAX_EPOCHS (5000)
+#define MAX_EPOCHS (2000)
+#define ITER_TRAINING_RATE (1)
+#define ITER_PRINTING_RATE (25)
 
 int main(void)
 {
     motor_command_t motors;
 
+    // Create line data for input, init iterator
     line_data_t line_data;
-    init_line_data_iter(1);
+    init_line_data_iter(ITER_TRAINING_RATE);
 
+    // Create and init network
     neural_net_t net;
     init_net(&net, LEARNING_RATE);
 
+    // Create outputs and 
     net_outputs_t outputs;
     infer_net(line_data, net, &outputs);
-
     print_net(net);
     print_results(line_data, outputs.output, motors);
 
@@ -35,21 +39,18 @@ int main(void)
                 infer_net(line_data, net, &outputs);
 
                 train_net(line_data, &net, motors);
-
-                
         }
         if (epoch % 100 == 0){
             print_error(epoch, motors, outputs);
         } 
     }
 
-
-    init_line_data_iter(10);
+    init_line_data_iter(ITER_PRINTING_RATE);
     while (continue_epoch()){
         line_data = get_line_iter();
         motors = compute_proportional(line_data.left, line_data.right);
         infer_net(line_data, net, &outputs);
-        print_results(line_data, outputs.output, motors);
+        chart_results(line_data, outputs.output, motors);
     }
     print_net(net);
     return 0;

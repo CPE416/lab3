a17674309a9340029ad800c41cd9b377f2c2b17a
Spencer Shaw <sfshaw@calpoly.edu>
2019-02-09 19:31:07 -0800

    clean up and slow printing in part3

--- lab3_part3.c
+++ lab3_part3.c
@@ -13,10 +13,6 @@
 #define DELAY_MS 200 // Delay time for loop
 #define DELAY_LOOP 500
 
-#define MODE_3A 1 
-#define MODE_3B 0
-
-#define DEAD_ZONE 200
 #define LEARNING_RATE 0.1
 
 #define MODE_PROP 0
@@ -40,15 +36,7 @@ int main(void)
     motor_command_t motors;
     int data_counter = 0;
 
-    motors.left = 0;
-    motors.right = 0;
     halt();
-
-    // u08 mode = set_mode(MODE_3A);
-    // u08 vals[] = {0, 0}; // Data retrieve from light sensors
-    // u08 data[] = {0, 0}; 
-    // Place text at start of buffer
-    // sprintf(buffer, "416");
 
     neural_net_t net;
     init_net(&net, LEARNING_RATE);
@@ -104,43 +92,10 @@ int main(void)
 
         // Delay
         // delay_us(DELAY_LOOP);
-        
-
-        // u08 left = data[0];
-        // u08 right = data[1];
-        // set_motors(left, right);
-
-        // print_4(vals[0], left, vals[1], right);
-        
-        // delay_ms(DELAY_MS);
-
-        // mode = set_mode(mode);
     }
     return 1;
 }
 
-void process_data_mode_a(u08* vals, u08* data){
-    data[0] = map_light_to_motor(vals[0]);
-    data[1] = map_light_to_motor(vals[1]);
-}
-
-void process_data_mode_b(u08* vals, u08* data){
-    data[0] = map_light_to_motor(vals[1]);
-    data[1] = map_light_to_motor(vals[0]);
-}
-
-u08 map_light_to_motor(u08 val){
-    if (val < DEAD_ZONE){
-        val = 0;
-    }else{
-        val = val - DEAD_ZONE;
-    } 
-    u08 data = (35 - val);
-    if (data > 100){
-        data = 0;
-    }
-    return data;
-}
 
 u08 set_mode(u08 mode){
     u08 pressed = ((get_btn() == 1) | (get_btn2() == 1));
@@ -158,35 +113,25 @@ u08 set_mode(u08 mode){
         delay_ms(200);
     }
     return mode;
-    // u08 pressed = get_btn();
-    // if(pressed){
-    //     if (mode == MODE_3A)
-    //         mode = MODE_3B;
-    //     else 
-    //         mode = MODE_3A;
-    // }
-    // switch (mode ){
-    //     case MODE_3A:
-    //         led_off(1);
-    //         led_on(0);
-    //         break;
-    //     case MODE_3B:
-    //         led_off(0);
-    //         led_on(1);
-    //         break;
-    //     }
-    // return mode;
 }
 
 void print_prop(int count){
+    if(!(count % 10 == 0)){
+        return;
+    }
     clear_screen();
+    lcd_cursor(0, 0);
     print_string("Prop");
     lcd_cursor(0, 1);
     print_num(count);
 }
 
 void print_neural(int count){
+    if(!(count % 10 == 0)){
+        return;
+    }
     clear_screen();
+    lcd_cursor(0, 0);
     print_string("Neural");
     lcd_cursor(0, 1);
     print_num(count);
@@ -194,6 +139,7 @@ void print_neural(int count){
 
 void print_training3(int count1, int count2){
     clear_screen();
+    lcd_cursor(0, 0);
     print_string("Training");
     lcd_cursor(0, 1);
     print_num(count1);
